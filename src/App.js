3de1bf53316147dc1550a4ee5665ad43bef05d76
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-05-30 19:14:30 +0900

    whole update static version (just wireframe)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react';
+// import './App.css';
 
-const App = () => {
-  return <h1>Hello React</h1>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='' id='quote-box'>
+        quote-box
+        <Author />
+        <NewQuote />
+        <TweetQuote />
+      </div>
+    );
+  }
+}
+
+class Author extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='' id='author'>
+        author
+      </div>
+    );
+  }
+}
+
+class NewQuote extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='' id='new-quote'>
+        new-quote
+      </div>
+    );
+  }
+}
+
+class TweetQuote extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <a
+        href='http://twitter.com/intent/tweet'
+        target='_blank'
+        rel='noopener noreferrer'
+        id='tweet-quote'
+      >
+        tweet
+      </a>
+    );
+  }
+}
 
 export default App;
