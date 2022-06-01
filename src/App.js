97a77c68db24d5129338e431e8279b09db9f5d5e
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-06-01 22:36:39 +0900

    rewrite App.js all

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import React, { Component } from 'react';
 import { Icon } from '@iconify/react';
 import './App.css';
 import Quotes from './quotes.json';
-import { render } from '@testing-library/react';
 const quotes = Quotes.quotes;
-
-function SetRandomeQuotes(key) {
-  console.log('test');
-  let index = Math.floor(Math.random() * quotes.length);
-  return quotes[index][key];
-}
 
 class App extends Component {
   constructor(props) {
@@ -26,83 +19,43 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ quote: SetRandomeQuotes('quote') });
-    this.setState({ author: SetRandomeQuotes('author') });
+    console.log('test for componentDidMount');
+    let index = Math.floor(Math.random() * quotes.length);
+    this.setState({ quote: quotes[index].quote });
+    this.setState({ author: quotes[index].author });
   }
 
-  render() {
-    return (
-      <div className='' id='quote-box'>
-        <Text quote={this.state.quote} />
-        <Author author={this.state.author} />
-        <NewQuote />
-        <TweetQuote quotes={quotes} />
-      </div>
-    );
-  }
-}
-
-class Text extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className='' id='text'>
-        {this.props.quote}
-      </div>
-    );
-  }
-}
-
-class Author extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className='' id='author'>
-        {this.props.author}
-      </div>
-    );
-  }
-}
-
-class NewQuote extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <button className='' id='new-quote'>
-        new-quote
-      </button>
-    );
-  }
-}
-
-class TweetQuote extends Component {
-  constructor(props) {
-    super(props);
+  TestOnClick() {
+    console.log('test for onclick method');
   }
 
   render() {
     const twitterUrl = 'http://twitter.com/intent/tweet';
     const hashtags = ['quote', 'freeCodeCamp'];
-    const text = this.props.quotes[0].quote;
+    const text = this.state.quote;
 
     return (
-      <p>
-        <a
-          href={`${twitterUrl}?text=${text}&hashtags=${hashtags}`}
-          target='_blank'
-          rel='noopener noreferrer'
-          id='tweet-quote'
-        >
-          <Icon icon='fa-brands:twitter-square' color='#1DA1F2' /> tweet
-        </a>
-      </p>
+      <div className='' id='quote-box'>
+        <div className='' id='text'>
+          {this.state.quote}
+        </div>
+        <div className='' id='author'>
+          {this.state.author}
+        </div>
+        <button className='' id='new-quote' onClick={this.TestOnClick}>
+          new-quote
+        </button>
+        <p>
+          <a
+            href={`${twitterUrl}?text=${this.state.quote}&hashtags=${hashtags}`}
+            target='_blank'
+            rel='noopener noreferrer'
+            id='tweet-quote'
+          >
+            <Icon icon='fa-brands:twitter-square' color='#1DA1F2' /> tweet
+          </a>
+        </p>
+      </div>
     );
   }
 }
