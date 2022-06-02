6989411ebb23a80588f8b7fc36faa4eb36cdc9ef
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-06-02 17:06:33 +0900

    update README (try to add changelog) update css, some functions, and so on. many , so many. but not finished.

--- src/App.js
+++ src/App.js
@@ -7,54 +7,53 @@ import React, { Component } from 'react';
 import { Icon } from '@iconify/react';
 import './App.css';
 import Quotes from './quotes.json';
+
 const quotes = Quotes.quotes;
+const index = Math.floor(Math.random() * quotes.length);
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: '',
-      author: '',
+      quote: quotes[index].quote,
+      author: quotes[index].author,
     };
   }
 
-  componentDidMount() {
-    console.log('test for componentDidMount');
-    let index = Math.floor(Math.random() * quotes.length);
-    this.setState({ quote: quotes[index].quote });
-    this.setState({ author: quotes[index].author });
-  }
-
-  TestOnClick() {
-    console.log('test for onclick method');
-  }
+  GetRandomQuote = () => {
+    // console.log('this is:', this);
+    const index = Math.floor(Math.random() * quotes.length);
+    this.setState((state) => ({
+      quote: quotes[index].quote,
+      author: quotes[index].author,
+    }));
+  };
 
   render() {
     const twitterUrl = 'http://twitter.com/intent/tweet';
     const hashtags = ['quote', 'freeCodeCamp'];
-    const text = this.state.quote;
 
     return (
-      <div className='' id='quote-box'>
-        <div className='' id='text'>
-          {this.state.quote}
+      <div id='quote-box'>
+        <div className='quote'>
+          <div id='text'>{this.state.quote}</div>
+          <div id='author'>{this.state.author}</div>
         </div>
-        <div className='' id='author'>
-          {this.state.author}
+        <div className='btn'>
+          <p>
+            <a
+              href={`${twitterUrl}?text=${this.state.quote}&hashtags=${hashtags}`}
+              target='_blank'
+              rel='noopener noreferrer'
+              id='tweet-quote'
+            >
+              <Icon icon='fa-brands:twitter-square' color='#1DA1F2' /> tweet
+            </a>
+          </p>
+          <button id='new-quote' onClick={this.GetRandomQuote}>
+            new-quote
+          </button>
         </div>
-        <button className='' id='new-quote' onClick={this.TestOnClick}>
-          new-quote
-        </button>
-        <p>
-          <a
-            href={`${twitterUrl}?text=${this.state.quote}&hashtags=${hashtags}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            id='tweet-quote'
-          >
-            <Icon icon='fa-brands:twitter-square' color='#1DA1F2' /> tweet
-          </a>
-        </p>
       </div>
     );
   }
