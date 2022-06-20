be4397e49ed1690c0e5821b8a2f6d0c4ccb37370
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-06-20 14:48:58 +0900

    add isLoaded state

--- src/App.js
+++ src/App.js
@@ -36,11 +36,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quoteData: Quotes.quotes,
-      index: SetRandomIndex(Quotes.quotes),
+      isLoaded: false,
     };
   }
-  componentDidMount() {}
+  componentDidMount() {
+    this.setState({
+      quoteData: Quotes.quotes,
+      index: SetRandomIndex(Quotes.quotes),
+      isLoaded: true,
+    });
+  }
 
   handleClick = () => {
     // renew state with function.
@@ -49,30 +54,36 @@ class App extends Component {
   };
 
   render() {
-    const quote = this.state.quoteData[this.state.index].quote;
+    if (this.state.isLoaded !== true) {
+      return <div>Now Loading...</div>;
+    } else {
+      const quote = this.state.quoteData[this.state.index].quote;
+      const author = this.state.quoteData[this.state.index].author;
+      return (
+        <div id='quote-box'>
+          <div className='contents'>
+            <h2 id='text'>{quote}</h2>
+            <p id='author'>{author}</p>
+          </div>
+          <div className='AxiosTest'>
+            AxiosTest
+            <h3>pls push button to run axios.get</h3>
+            <button onClick={() => TestAxios()}>TestAxios</button>
+          </div>
 
-    const author = this.state.quoteData[this.state.index].author;
-    return (
-      <div id='quote-box'>
-        <div className='contents'>
-          <h2 id='text'>{quote}</h2>
-          <p id='author'>{author}</p>
+          <div className='buttons'>
+            <Tweet text={quote} />
+            <Button
+              variant='contained'
+              id='new-quote'
+              onClick={this.handleClick}
+            >
+              CHANGE QUOTE
+            </Button>
+          </div>
         </div>
-        <div className='AxiosTest'>
-          AxiosTest
-          <h3>pls push button to run axios.get</h3>
-          <button onClick={() => TestAxios()}>TestAxios</button>
-        </div>
-
-        <div className='buttons'>
-          <Tweet text={quote} />
-          <Button variant='contained' id='new-quote' onClick={this.handleClick}>
-            CHANGE QUOTE
-          </Button>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
-
 export default App;
